51c4a0b7bb73a11d2fcb974d4e05643b68a2075b
CnqC <130760728+CnqC@users.noreply.github.com>
2023-06-28 17:00:05 +0700

    Create GameControllerPad script
    Create GameControllerPad script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnqC.PlatformGame;
+
+public class GameManager : SingleTon<GameManager>
+{
+    public GamePlaySetting setting;
+    public override void Awake()
+    {
+        // base.Awake();  hủy khi load scene 
+
+        MakeSingleTon(false); // hủy gameObject khi load sang scene khác
+    }
+}
