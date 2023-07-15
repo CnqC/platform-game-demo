0392bc020548bdf3b9514fdb7a3072b9326f68c6
CnqC <130760728+CnqC@users.noreply.github.com>
2023-07-15 18:40:33 +0700

    Make Player can shot
    Make Player can shot

---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnqC.PlatformGame;
+
+public class FireBullet : MonoBehaviour
+{
+    public Player player;
+    public Transform firePoint;
+    public Bullet bulletPb;
+
+    private float m_curSpeed;
+
+    public void Fire()
+    {
+        if (!bulletPb || !player || !firePoint) return;
+
+        // check xem player có quay mặt sang trái k, nếu có sang trái thì phải - speed để có thể cùng hướng trái
+        m_curSpeed = player.IsFacingLeft == true ? -(bulletPb.speed) : bulletPb.speed;
+
+        // tạo ra player trên scene
+        var bulletClone = Instantiate(bulletPb, firePoint.position,Quaternion.identity);
+        bulletClone.speed = m_curSpeed;
+        bulletClone.owner = player;
+
+        // giảm số lượng đạn xuống 1 đơn vị
+    
+    }
+}
