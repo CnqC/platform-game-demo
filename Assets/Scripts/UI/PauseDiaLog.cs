0816d2ec78e924295a2df350e58b8c90cee7240a
CnqC <130760728+CnqC@users.noreply.github.com>
2023-12-19 19:39:02 +0700

    Create script to handle PauseDiaLog
    Create script to handle PauseDiaLog

---
+++ Assets/Scripts/UI/PauseDiaLog.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnqC.PlatformGame;
+
+public class PauseDiaLog : Dialog
+{
+    public override void Show(bool isShow)
+    {
+        Time.timeScale = 0f; // dừng tất cả mọi thứ lại
+        base.Show(isShow);
+
+        
+
+    }
+
+    public void Replay()
+    {
+        // restart lại map đó
+        Close();
+        SceneController.Ins.LoadLevelScene(LevelManager.Ins.CurlevelId);
+    }
+
+    public void OpenSetting()
+    {
+        if(GUIManager.Ins.settingDiaLog != null)
+        {
+            GUIManager.Ins.settingDiaLog.Show(true); // open SettingDiaLog
+        }
+    }
+
+    public void Exit()
+    {
+        Close(); // đóng hộp thoại
+        SceneController.Ins.LoadScene("MainMenu");
+    }
+    public override void Close()
+    {
+        // stop pause
+        Time.timeScale = 1f;
+        base.Close();
+
+    }
+}
---
+++ Assets/Scripts/UI/Setting DiaLog.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnqC.PlatformGame;
+
+public class SettingDiaLog : Dialog
+{
+    // xử lý hộp thoại setting
+}
